648293d51a36fe5ebc24def7a3100bcfe55c9cbd
Michael Kuznetsov <makuznetsov_3@edu.hse.ru>
2020-03-27 20:23:03 +0300

    test/ nothing works

--- src/main/java/ru/hse/edu/myurachinskiy/Main.java
+++ src/main/java/ru/hse/edu/myurachinskiy/Main.java
@@ -1,6 +1,10 @@
 package ru.hse.edu.myurachinskiy;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,6 +14,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-
+        FXMLLoader loader = new FXMLLoader();
+        loader.setController(new MainSceneController());
+        loader.setLocation(getClass().getResource("keyboard-layout.fxml"));
+        primaryStage.setTitle("Hello world Application");
+        /*Label helloWorldLabel = new Label("Hello world!");
+        helloWorldLabel.setAlignment(Pos.CENTER);
+        Scene primaryScene = new Scene(helloWorldLabel);
+        primaryStage.setScene(primaryScene);
+        */primaryStage.show();
     }
 }
---
+++ src/main/java/ru/hse/edu/myurachinskiy/MainSceneController.java
@@ -0,0 +1,13 @@
+package ru.hse.edu.myurachinskiy;
+
+import javafx.fxml.FXML;
+
+public class MainSceneController {
+    
+    MainSceneController() {}
+    
+    @FXML
+    public void buttonClicked() {
+        System.out.println("Button clicked!");
+    }
+}
